45d212a43fd1faed5a9e83b2a8b7c27e25b1e770
Alecaring <Private_a.a.c@icloud.com>
2024-05-20 15:40:04 +0200

    first commit

---
+++ form-controls.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <form action="CensBadWords">
+
+        <label for="txtArea">Insert Your Text</label>
+        <textarea name="txtArea" id="txtArea" cols="30">bla</textarea>
+
+        <label for="badWords">Insert Here The Word To Censor</label>
+        <input type="badWords" id="badWords">
+
+    </form>
+    
+</body>
+
+</html>
